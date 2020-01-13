29cc19d4faf675ea0e98cbf4f7271adabf2da5e8
eballabe <eric.ballabene@cern.ch>
2020-01-13 09:10:53 +0100

    Removed build folder

--- build/ParticleAnalysis/CMakeFiles/Package_ParticleAnalysis.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../x86_64-centos7-gcc8-opt/include/ParticleAnalysis"
-  "CMakeFiles/Package_ParticleAnalysis"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Package_ParticleAnalysis.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/ParticleAnalysis/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /afs/cern.ch/user/e/eballabe/public/ntuple/source/ParticleAnalysis
-# Build directory: /afs/cern.ch/user/e/eballabe/public/ntuple/build/ParticleAnalysis
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
